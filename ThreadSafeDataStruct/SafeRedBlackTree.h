7d60c873499bd9b371d546f63e6cd7aaf9229239
dengzheng <dengzheng2008@qq.com>
2024-04-09 14:45:16 +0800

    完成接口定义；修改为模版。修改指针类型为shared_ptr解决节点失效问题。

--- ThreadSafeDataStruct/SafeRedBlackTree.h
+++ ThreadSafeDataStruct/SafeRedBlackTree.h
@@ -1,20 +1,235 @@
-struct node
-{
-    node();
-    node( const node& );
-    ~node();
-    node* m_left;
-    node* m_right;
-    int   m_data;
-};
+#include <atomic>
+#include <memory>
+#include <tuple>
 
+template < typename key, typename value >
 class RedBlackTree
 {
+    class node
+    {
+      public:
+        node();
+        node( const node& val );
+        node( node&& val );
+        node& operator=( const node& val );
+        node& operator=( node&& val );
+        ~node();
+        auto operator<=>( const node&& ) const;
+
+      private:
+        std::shared_ptr< node > m_left;
+        std::shared_ptr< node > m_right;
+        value                   m_value;
+        key                     m_key;
+    };
+
+    class iterator
+    {
+      public:
+        iterator();
+        iterator( const iterator& );
+        iterator( iterator&& );
+        iterator& operator=( const iterator& );
+        iterator& operator=( iterator&& );
+        ~iterator();
+        iterator&                 operator++( int );
+        iterator&                 operator++();
+        bool                      operator==( const iterator&& ) const noexcept;
+        bool                      operator!=( const iterator&& ) const noexcept;
+        std::pair< key&, value& > operator*();
+
+      private:
+        std::shared_ptr< node > m_NowNode;
+    };
+
   public:
     RedBlackTree();
     ~RedBlackTree();
-    void insert();
+    void     insert( std::pair< key, value > value );
+    value&   operator[]( const key&& key );
+    iterator find( const key&& key );
+    iterator begin();
+    iterator end();
+    void     eraser( const iterator& );
+    void     clear();
+    size_t   size();
 
   private:
-    node* m_root;
+    /*
+    m_root：
+            左孩子永远指向最小值
+            右孩子永远指向最大值
+            父节点永远指向头结点
+     */
+    node               m_root;
+    std::atomic_size_t m_size;
 };
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::node::node()
+{
+    m_left  = nullptr;
+    m_right = nullptr;
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::node::node( const node& val )
+{
+    m_key   = val.m_key;
+    m_value = val.m_value;
+    m_left  = val.m_left;
+    m_right = val.m_right;
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::node::node( node&& val )
+{
+    std::swap( m_left, val.m_left );
+    std::swap( m_right, val.m_right );
+    std::swap( m_value, val.m_value );
+    std::swap( m_key, val.m_key );
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::node& RedBlackTree< key, value >::node::operator=( const node& val )
+{
+    m_key   = val.m_key;
+    m_value = val.m_value;
+    m_left  = val.m_left;
+    m_right = val.m_right;
+    return *this;
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::node& RedBlackTree< key, value >::node::operator=( node&& val )
+{
+    std::swap( m_left, val.m_left );
+    std::swap( m_right, val.m_right );
+    std::swap( m_value, val.m_value );
+    std::swap( m_key, val.m_key );
+    return *this;
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::node::~node()
+{
+}
+
+template < typename key, typename value >
+inline auto RedBlackTree< key, value >::node::operator<=>( const node&& val ) const
+{
+    return m_key <=> val.m_key;
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::RedBlackTree()
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::~RedBlackTree()
+{
+}
+
+template < typename key, typename value >
+inline void RedBlackTree< key, value >::insert( std::pair< key, value > value )
+{
+}
+
+template < typename key, typename value >
+inline value& RedBlackTree< key, value >::operator[]( const key&& key )
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator RedBlackTree< key, value >::find( const key&& key )
+{
+    return iterator();
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator RedBlackTree< key, value >::begin()
+{
+    return iterator();
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator RedBlackTree< key, value >::end()
+{
+    return iterator();
+}
+
+template < typename key, typename value >
+inline void RedBlackTree< key, value >::eraser( const iterator& )
+{
+}
+
+template < typename key, typename value >
+inline void RedBlackTree< key, value >::clear()
+{
+}
+
+template < typename key, typename value >
+inline size_t RedBlackTree< key, value >::size()
+{
+    return size_t();
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator::iterator()
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator::iterator( const iterator& )
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator::iterator( iterator&& )
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator& RedBlackTree< key, value >::iterator::operator=( const iterator& )
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator& RedBlackTree< key, value >::iterator::operator=( iterator&& )
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator::~iterator()
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator& RedBlackTree< key, value >::iterator::operator++( int )
+{
+}
+
+template < typename key, typename value >
+inline RedBlackTree< key, value >::iterator& RedBlackTree< key, value >::iterator::operator++()
+{
+    // TODO: 在此处插入 return 语句
+}
+
+template < typename key, typename value >
+inline bool RedBlackTree< key, value >::iterator::operator==( const iterator&& ) const noexcept
+{
+    return false;
+}
+
+template < typename key, typename value >
+inline bool RedBlackTree< key, value >::iterator::operator!=( const iterator&& ) const noexcept
+{
+    return false;
+}
+
+template < typename key, typename value >
+inline std::pair< key&, value& > RedBlackTree< key, value >::iterator::operator*()
+{
+    return std::pair< key&, value& >();
+}
